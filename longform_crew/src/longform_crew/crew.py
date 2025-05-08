a71e18eaf850946e458ba4e769c24e0f1bca039a
Wesley Ordoñez <wesordonez1@gmail.com>
2025-05-08 17:16:30 -0500

    installed crewai into venv, updated reqs text, updated crew.py with base structure

--- longform_crew/src/longform_crew/crew.py
+++ longform_crew/src/longform_crew/crew.py
@@ -1,5 +1,6 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool, WebScraperTool
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
 # If you want to run a snippet of code before or after the crew starts,
@@ -7,58 +8,123 @@ from typing import List
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 @CrewBase
-class LongformCrew():
-    """LongformCrew crew"""
-
+class WebContentCreationCrew():
+    """Web Content Creation crew for generating blog posts, LinkedIn content, and newsletters"""
     agents: List[BaseAgent]
     tasks: List[Task]
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
+    @agent
+    def content_manager(self) -> Agent:
+        return Agent(
+            config=self.agents_config['content_manager'],  # type: ignore[index]
+            verbose=True
+        )
+
     @agent
     def researcher(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['researcher'],  # type: ignore[index]
+            verbose=True,
+            tools=[SerperDevTool(), WebScraperTool()]  # Use both Serper for search and WebScraper for detailed content
+        )
+
+    @agent
+    def blog_writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['blog_writer'],  # type: ignore[index]
             verbose=True
         )
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def linkedin_writer(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            config=self.agents_config['linkedin_writer'],  # type: ignore[index]
             verbose=True
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
-    @task
-    def research_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+    @agent
+    def newsletter_writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['newsletter_writer'],  # type: ignore[index]
+            verbose=True
+        )
+
+    @agent
+    def editor(self) -> Agent:
+        return Agent(
+            config=self.agents_config['editor'],  # type: ignore[index]
+            verbose=True
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def content_planning_task(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
+            config=self.tasks_config['content_planning_task'],  # type: ignore[index]
+            output_file='output/content_plan.md'
+        )
+
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'],  # type: ignore[index]
+            output_file='output/research_brief.md'
+        )
+
+    @task
+    def blog_writing_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['blog_writing_task'],  # type: ignore[index]
+            output_file='output/blog_post.md'
+        )
+
+    @task
+    def linkedin_writing_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['linkedin_writing_task'],  # type: ignore[index]
+            output_file='output/linkedin_post.md'
+        )
+
+    @task
+    def newsletter_writing_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['newsletter_writing_task'],  # type: ignore[index]
+            output_file='output/newsletter_content.md'
+        )
+
+    @task
+    def blog_editing_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['blog_editing_task'],  # type: ignore[index]
+            output_file='output/blog_post_final.md'
+        )
+
+    @task
+    def linkedin_editing_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['linkedin_editing_task'],  # type: ignore[index]
+            output_file='output/linkedin_post_final.md'
+        )
+
+    @task
+    def newsletter_editing_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['newsletter_editing_task'],  # type: ignore[index]
+            output_file='output/newsletter_content_final.md'
+        )
+
+    @task
+    def final_review_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['final_review_task'],  # type: ignore[index]
+            output_file='output/final_review.md'
         )
 
     @crew
     def crew(self) -> Crew:
-        """Creates the LongformCrew crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
+        """Creates the Web Content Creation crew"""
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.sequential,
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,  # We want the tasks to be executed in sequence
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
